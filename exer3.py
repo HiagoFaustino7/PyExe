b04e2617baa37af1ea53c121a764f74135ac6894
HiagoFaustino7 <hiago.lima@soulasalle.com.br>
2024-04-22 00:44:46 -0300

    exercicio 3 completo

---
+++ exer3.py
@@ -0,0 +1,8 @@
+# 3. Crie um script Python que leia o dia, mês e ano de nascimento de uma pessoa e
+# mostre uma mensagem com a data formatada.
+
+dia = int(input("Dia de nascimento: "))
+mes = int(input("Mês do nascimento: "))
+ano = int(input("Ano do nascimento: "))
+
+print(f"Formatado: {dia}/{mes}/{ano}")
