0137ae8844965d4d4efc92adb0c65f25b78f9938
HiagoFaustino7 <hiago.lima@soulasalle.com.br>
2024-04-23 20:08:41 -0300

    exer 2 completo

---
+++ TDE3/exer2.py
@@ -0,0 +1,9 @@
+# 2o Exercício
+# Crie um programa em linguagem Python que receba um número, conte o número
+# total de dígitos e mostre o resultado. Por exemplo, se o número é 201 , então a
+# saída deve ser 3
+
+
+numero = input("Digite um número: ")
+total_digitos = len(numero)
+print("O número de dígitos é:", total_digitos)
