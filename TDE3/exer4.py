e91a002bb0aba8927cd52b57f4dd19b60a8ba01f
HiagoFaustino7 <hiago.lima@soulasalle.com.br>
2024-04-23 20:22:03 -0300

    exer 4 completo

---
+++ TDE3/exer4.py
@@ -0,0 +1,13 @@
+# 4o Exercício
+# Crie um programa em Linguagem Python que solicite a senha de um usuário e
+# depois, peça para digitar novamente até que as duas senhas sejam
+# correspondentes.
+
+usuario = input("Digite o seu usuário: ")
+senha = input("Digite sua senha: ")
+csenha = input("Confirme a senha: ")
+
+while senha != csenha:
+    csenha = input("A senha não é igual a anterior, digite novamente: ")
+
+print("Cadastro finalizado")
