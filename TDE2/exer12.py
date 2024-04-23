d6d26279dd9fcb1f753359ef0be7effb9b3e2352
HiagoFaustino7 <hiago.lima@soulasalle.com.br>
2024-04-23 20:04:36 -0300

    inicio a parte 3

--- TDE2/exer12.py
+++ TDE2/exer12.py
@@ -1,6 +1,6 @@
 # 12. Faça um programa que calcule as raízes de uma equação do segundo grau,
 # na forma a2 + b + c. O programa deverá pedir os valores de a, b e c e fazer
-# os testes necessários.
+# os testes necessários...
 
 a = float(input("Digite o valor de A: "))
 b = float(input("Digite o valor de B: "))
