de6b15537b02ad462310360ee05c02fa81d61711
HiagoFaustino7 <hiago.lima@soulasalle.com.br>
2024-04-23 15:49:21 -0300

    exer 9 finished

---
+++ TDE2/exer9.py
@@ -0,0 +1,12 @@
+# 9. Faça um programa usando o for que leia um nome de usuário e a sua senha
+# e não aceite a senha igual ao nome do usuário, mostrando uma mensagem
+# de erro e voltando a pedir as informações.
+
+for _ in range(9999999999):
+    nome = input("Digite seu nome de usuário: ")
+    senha = input("Digite sua senha: ")
+
+    if nome == senha:
+        print("O nome de usuario não pode ser igual a senha.")
+    else:
+        break
