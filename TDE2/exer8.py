bccd384a4b4ee76745407275d19179068114818e
HiagoFaustino7 <hiago.lima@soulasalle.com.br>
2024-04-23 15:20:44 -0300

    exer 8 finished

--- TDE2/exer8.py
+++ TDE2/exer8.py
@@ -1,5 +1,17 @@
-# 7. Faça um
+# 8. Faça um
 # # programa usando o for que peça uma nota, entre zero e dez. Mostre uma
 # # mensagem caso o valor seja inválido e continue pedindo até que o usuário
 # # informe um valor válido.
+
+
+
+
+
+for _ in range(999999):
+    nota = float(input("Digite sua nota, entre 0 e 10: "))
+    if nota >= 0 and nota <= 10:
+        print(f"Nota válida: {nota}")
+        break
+    else:
+        print('Nota invalida!')
 
