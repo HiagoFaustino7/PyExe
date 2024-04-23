f5ca06679a615a08f770e11de86be4ef7a7f74b9
HiagoFaustino7 <hiago.lima@soulasalle.com.br>
2024-04-23 14:20:57 -0300

    exer 5 finished

---
+++ TDE2/exer6.py
@@ -0,0 +1,18 @@
+# 6. Dada uma letra, escreva na tela se essa letra é uma vogal ou consoante
+# (considerar apenas letras minúsculas).
+
+letra = input('Digite uma letra: ').lower()
+
+
+if letra.isalpha() and len(letra) == 1:
+
+    if letra in 'aeiou':
+        print('É uma vogal')
+
+    else:
+        print('É uma consoante')
+
+
+else:
+
+    print('Você digitou mais de uma letra, ou algum número, renicie o programa')
